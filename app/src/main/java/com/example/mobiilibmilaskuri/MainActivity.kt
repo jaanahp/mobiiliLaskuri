1047b61f8a8617ac5777cf520eca58457e82dfd0
jaanahp <pusa.jaana@gmail.com>
2024-01-25 14:19:34 +0200

    Added the checkbox, which disables and enables "Laske"-button. Changed the "Laske" button to call on a function, which calculates the BMI.

--- app/src/main/java/com/example/mobiilibmilaskuri/MainActivity.kt
+++ app/src/main/java/com/example/mobiilibmilaskuri/MainActivity.kt
@@ -3,9 +3,13 @@ package com.example.mobiilibmilaskuri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,8 +19,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mobiilibmilaskuri.ui.theme.MobiiliBMIlaskuriTheme
 import kotlin.math.pow
 
@@ -41,19 +49,57 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     var height by remember { mutableStateOf("")}
     var weight by remember { mutableStateOf("")}
-    var bmi = if (height != "" && weight != "") (1.3 * weight.toDouble()) / ((height.toDouble() / 100).pow(2.5)) else "Arvo ei voi olla nolla"
-    Column{
-        TextField(value = "", onValueChange = {height = it},
+    var bmi by remember { mutableStateOf("")}
+    var buttonEnabled by remember { mutableStateOf(false)}
+
+    fun calculateBmi() {
+        val heightDouble = height.toDouble()
+        val weightDouble = weight.toDouble()
+        bmi = if (height != "" && weight != "") {
+            ((1.3 * weightDouble) / ((heightDouble / 100).pow(2.5))).toString()
+        } else "Arvo ei voi olla tyhjä"
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = height,
+            onValueChange = {height = it},
+            modifier = Modifier.padding(top = 8.dp),
+            textStyle = TextStyle.Default.copy(fontSize = 28.sp),
             label = { Text("Pituus (cm)")})
-        TextField(value = "", onValueChange = {weight = it},
+        TextField(
+            value = weight,
+            onValueChange = {weight = it},
+            modifier = Modifier.padding(top = 8.dp),
+            textStyle = TextStyle.Default.copy(fontSize = 28.sp),
             label = { Text("Paino (kg)")})
-        Button(onClick = {
-            height = "176"
-            weight = "86.8"
-        }) {
+        Text(
+            text = bmi,
+            modifier = Modifier.padding(top = 8.dp),
+            fontSize = 26.sp
+        )
+        Row {
+        Checkbox(
+            checked = buttonEnabled,
+            onCheckedChange = {buttonEnabled = it},
+            modifier = Modifier.padding(top = 8.dp),
+            )
+            Text(
+                text = "Olen aikuinen",
+                modifier = Modifier.padding(9.dp))
+        }
+
+        Button(
+            onClick = { calculateBmi() },
+            modifier = Modifier.padding(top = 8.dp),
+            enabled = buttonEnabled
+        ) {
             Text(text = "Laske")
         }
-        Text(text = bmi.toString())
     }
 }
 
